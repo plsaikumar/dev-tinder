0f542daf58c6c67e0474c8c4f7ef35c1778c3e2f
plsaikumar <plsaikumar72@gmail.com>
2025-03-05 13:52:31 +0530

    routing logics

--- src/app.js
+++ src/app.js
@@ -1,13 +1,32 @@
 const express = require("express");
 const app = express();
 
-app.use("/hello", (req, res) => {
+app.get("/hello", (req, res) => {
   res.send("Hello from server");
 });
 
-app.use("/", (req, res) => {
+app.get("/", (req, res) => {
   res.send("Intial route from server");
 });
+app.get("/u(se)?r",(req,res)=>{
+    res.send("Data posted successfully")
+})
+app.post("/us+er",(req,res)=>{
+    res.send("Data posted successfully")
+})
+
+app.post("/us*er",(req,res)=>{
+    res.send("Data posted successfully")
+})
+app.get("/u(se)?r",(req,res)=>{
+    res.send("Data posted successfully")
+})
+app.get("/u(se)+r",(req,res)=>{
+    res.send("Data posted successfully")
+})
+app.get("/u(se)*r",(req,res)=>{
+    res.send("Data posted successfully")
+})
 app.listen(3000, () => {
   console.log("Success");
 });
